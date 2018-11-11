5f3a46babf655408423dba624f898b4a8ca19037
lais.lopes <lais.lopes@setup.com.br>
2018-11-11 13:06:21 -0200

    Criada e testada conexão com o Banco de Dados bytebank.

--- CadastroBD/src/model/dao/PessoaFisicaDAO.java
+++ CadastroBD/src/model/dao/PessoaFisicaDAO.java
@@ -5,10 +5,29 @@
  */
 package model.dao;
 
+import connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author danie
  */
 public class PessoaFisicaDAO {
+    
+    public void create(){
+        
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        
+        try {
+            stmt = con.prepareStatement("");
+        } catch (SQLException ex) {
+            Logger.getLogger(PessoaFisicaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
