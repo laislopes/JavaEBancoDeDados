11a6231da65caac92a4b337713b8c60d8be1660d
lais.lopes <lais.lopes@setup.com.br>
2018-11-16 01:26:05 -0200

    Add configurações persistence.xml e anotações na Classe Categoria.

--- JavaJPA/src/model/bean/Categoria.java
+++ JavaJPA/src/model/bean/Categoria.java
@@ -1,8 +1,16 @@
 
 package model.bean;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String descricao;
 
